35bf31a1315f3fbdd738f14df2a5326039e12376
lgants <lgants@gmail.com>
2017-08-22 19:31:50 -0500

    added category component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCategories } from '../actions';
 
 
 class App extends Component {
   componentDidMount() {
-    fetch('http://jservice.io/api/categories?count=20')
-      .then(response => response.json())
-      .then(json => {
-        this.props.setCategories(json);
-      });
+    if (this.props.categories.length === 0) {
+      fetch('http://jservice.io/api/categories?count=20')
+        .then(response => response.json())
+        .then(json => this.props.setCategories(json));
+    }
   }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>Jeopardy</h2>
+        {
+          this.props.categories.map(category => {
+            return (
+              <div key={category.id}>
+                <Link to="/category"><h4>{category.title}</h4></Link>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import rootReducer from './reducers';
 import App from './components/App';
+import Category from './components/Category';
+import './index.css';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/category" component={Category} />
+        </Switch>
+      </BrowserRouter>
   </Provider>,
 document.getElementById('root'));
